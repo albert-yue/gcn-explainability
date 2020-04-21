Add stopword pruning, adj matrix normalization, and fix dimension problems

src/data.py
@@ -1,4 +1,5 @@
 import random
+import torch
 from torch.utils.data import Dataset
 
 UNKNOWN_TOKEN = '<unk>'
@@ -26,7 +27,7 @@ class Corpus(Dataset):
         random.shuffle(self.data)
     
     def labels(self):
-        return [doc.label for doc in self.data]
+        return torch.LongTensor([doc.label for doc in self.data])
 
 
 def get_vocabulary(vocab_path):
@@ -58,7 +59,7 @@ def get_data(data_path, all_labels):
         for line in f.readlines():
             line = line.strip()
             label, text = line.split('\t')
-            doc = Document(text, label_to_index[label])
+            doc = Document(text.split(), label_to_index[label])
             documents.append(doc)
     
     return Corpus(documents)
@@ -70,7 +71,7 @@ def save_all_labels(out_path, train_path, test_path):
         with open(data_path, 'r') as f:
             for line in f.readlines():
                 line = line.strip()
-                label, text = line.split('\t')
+                label, _ = line.split('\t')
                 labels.add(label)
     labels = list(labels)
     with open(out_path, 'w+') as f:

src/models/gcn.py
@@ -36,12 +36,11 @@ class GraphConv(nn.Module):
 
 
 class GCN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, vocab_size, dropout=0.):
+    def __init__(self, input_size, hidden_size, output_size, dropout=0.):
         super(GCN, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
-        self.vocab_size = vocab_size
 
         self.dropout = nn.Dropout(dropout)
         self.act_func = nn.ReLU()
@@ -55,7 +54,6 @@ class GCN(nn.Module):
         out = self.act_func(self.layer1(adj_matrix, inp))
         out = self.dropout(out)
         out = self.layer2(adj_matrix, out)
-        out = out[vocab_size:, :]  # get rows for documents
         
         if not self.training:
             out = self.softmax(out)

src/preprocessing.py
@@ -1,26 +1,23 @@
 from collections import defaultdict
 from math import log
 
+from nltk.corpus import stopwords
 import torch
 import torch.sparse as sparse
-from tqdm.auto import tqdm
+from tqdm.auto import tqdm, trange
 
 UNKNOWN_TOKEN = '<unk>'
 
 
-def mask_unknown_words(corpus, vocabulary):
-    masked = set()
+def clean_text(corpus, vocabulary):
+    stop_words = stopwords.words('english')
     for doc in tqdm(corpus):
-        words = doc.text.split()
-        for i, word in enumerate(words):
-            if word not in vocabulary:
-                masked.add(word)
-                words[i] = UNKNOWN_TOKEN
-        doc.text = ' '.join(words)
-    print('Masked {} distinct unknown words'.format(len(masked)))
+        words = doc.text
+        doc.text = [(w if w in vocabulary else UNKNOWN_TOKEN) for w in words if w not in stop_words]
+    return
 
 
-def build_adj_matrix(corpus, vocabulary, window_size=20):
+def build_adj_matrix(corpus, vocabulary, num_documents, doc_offset=0, window_size=20):
     """
     Builds the adjacency matrix A for the text-document graph
     
@@ -31,7 +28,6 @@ def build_adj_matrix(corpus, vocabulary, window_size=20):
     
     """
     num_words = len(vocabulary)
-    num_documents = len(corpus)
     num_vertices = num_words + num_documents
 
     word_to_index = {w: i for i, w in enumerate(vocabulary)}
@@ -42,12 +38,13 @@ def build_adj_matrix(corpus, vocabulary, window_size=20):
     row = []
     col = []
 
+    print('Building word frequencies per doc')
     word_freqs_per_doc = {}
-    for i, doc in enumerate(corpus):
-        doc_idx = num_words + i
+    for i, doc in enumerate(tqdm(corpus)):
+        doc_idx = num_words + doc_offset + i
 
         word_freq = {}
-        text = doc.text.split()
+        text = doc.text
         for word in text:
             if word not in vocabulary:
                 word = '<unk>'
@@ -60,12 +57,13 @@ def build_adj_matrix(corpus, vocabulary, window_size=20):
         word_freqs_per_doc[doc_idx] = word_freq
     
     ### PMI calculations
+    print('Building word frequencies per window')
     num_windows = 0
     word_window_occurrences = defaultdict(int)
     word_pair_window_occurrences = defaultdict(int)  # keys are tuples (x,y) where x < y are strings
-    for doc in corpus:
-        text = doc.text.split()
-        for window_start_idx in range(0, max(1, len(text) - window_size + 1)):
+    for doc in tqdm(corpus):
+        text = doc.text
+        for window_start_idx in range(max(1, len(text) - window_size + 1)):
             num_windows += 1
             window = text[window_start_idx:window_start_idx + window_size]
             distinct_words = list(set(window))
@@ -83,7 +81,8 @@ def build_adj_matrix(corpus, vocabulary, window_size=20):
     word_window_occurrences = dict(word_window_occurrences)
     word_pair_window_occurrences = dict(word_pair_window_occurrences)
     
-    for pair, pair_freq in word_pair_window_occurrences.items():
+    print('Calculating PMIs')
+    for pair, pair_freq in tqdm(word_pair_window_occurrences.items()):
         word1, word2 = pair
         w1_idx, w2_idx = word_to_index[word1], word_to_index[word2]
         freq1 = word_window_occurrences[word1]
@@ -103,7 +102,8 @@ def build_adj_matrix(corpus, vocabulary, window_size=20):
         col.append(w1_idx)
 
     ### TF-IDF calculations
-    for w_idx, word in enumerate(vocabulary):
+    print('Calculating TF-IDF')
+    for w_idx, word in enumerate(tqdm(vocabulary)):
         doc_occurrences = 0
         for doc_idx, freqs in word_freqs_per_doc.items():
             if word in freqs:
@@ -123,7 +123,8 @@ def build_adj_matrix(corpus, vocabulary, window_size=20):
                 col.append(w_idx)
 
     ### 1s for identities
-    for i in range(num_vertices):
+    print('Identities')
+    for i in trange(num_vertices):
         entries.append(1)
         row.append(i)
         col.append(i)
@@ -138,7 +139,7 @@ def build_adj_matrix(corpus, vocabulary, window_size=20):
 #     adj = torch.eye(num_vertices)
 #     for i, doc in enumerate(corpus):
 #         adj_idx = len(vocabulary) + i
-#         text = doc.text.split()
+#         text = doc.text
 #         for word in text:
 
 if __name__ == '__main__':
@@ -146,6 +147,6 @@ if __name__ == '__main__':
     vocab = get_vocabulary('data/20ng-vocabulary.txt')
     labels = get_labels('data/20ng-labels.txt')
     corpus = get_data('data/test.txt', labels)
-    mask_unknown_words(corpus, vocab)
+    clean_text(corpus, vocab)
     adj = build_adj_matrix(corpus, vocab)
     print(adj)

src/train.py
@@ -4,32 +4,33 @@ import torch.nn as nn
 from torch.optim import Adam
 
 from src.data import Corpus, get_data, get_vocabulary, get_labels
-from src.preprocessing import mask_unknown_words, build_adj_matrix
+from src.preprocessing import clean_text, build_adj_matrix
 
 
-def evaluate(model, adj_matrix, targets, metric_fn):
+def evaluate(model, adj_matrix, targets, metric_fn, start_idx=None, end_idx=None):
     model.eval()
     with torch.no_grad():
         preds = model(adj_matrix)
+    
+    if start_idx is None: start_idx = 0
+    if end_idx is None: end_idx = preds.size(0)
+    preds = preds[start_idx:end_idx, :]
+
     return metric_fn(preds, targets)
 
 
-def train(model, corpus, epochs=100, init_lr=0.001, val_split=0.1, val_every=1, seed=None, print_every=10, plot_every=5, save_path='logs/train.pt'):
-    # Split validation set
-    corpus.shuffle(seed)
-    len_train = int(len(corpus) * (1 - val_split))
-    train_corpus = Corpus(corpus[:len_train])
-    val_corpus = Corpus(corpus[len_train:])
-
-    train_adj_matrix = build_adj_matrix(train_corpus, vocabulary)
-    val_adj_matrix = build_adj_matrix(val_corpus, vocabulary)
-
+def train(model, train_adj_matrix, val_adj_matrix, train_labels, val_labels, vocab_size, epochs=100, init_lr=0.001, val_every=1, print_every=10, plot_every=5, save_path='logs/train.pt'):
     loss_fn = nn.CrossEntropyLoss()
     optim = Adam(model.parameters(), lr=init_lr)
+
+    train_size, val_size = len(train_labels), len(val_labels)
+    val_start_idx = vocab_size + train_size
+    test_start_idx = vocab_size + train_size + val_size
 
     train_losses = []
     val_losses = []
     best_val_loss = float('inf')
+    # epochs_with_no_improvement = 0
     for epoch in range(epochs):
         if epoch % print_every == 0:
             print('Epoch', epoch)
@@ -38,53 +39,75 @@ def train(model, corpus, epochs=100, init_lr=0.001, val_split=0.1, val_every=1, 
         model.zero_grad()
 
         out = model(train_adj_matrix)
-        loss = loss_fn(out, train_corpus.labels())
+        out = out[vocab_size:val_start_idx, :]
+        loss = loss_fn(out, train_labels)
         loss.backward()
         optim.step()
 
         train_losses.append(loss.data.item())
         if epoch % print_every == 0:
-            print('Train mean cross-entropy:', val_loss)
+            print('Train mean cross-entropy:', loss.item())
 
         if epoch % val_every == 0:
-            val_loss = evaluate(model, val_adj_matrix, val_corpus.labels(), loss_fn)
+            val_loss = evaluate(model, val_adj_matrix, val_labels, loss_fn, start_idx=val_start_idx, end_idx=test_start_idx)
             if val_loss <= best_val_loss:
                 torch.save(model.state_dict(), save_path)
             
             val_losses.append(val_loss.data.item())
             if epoch % print_every == 0:
-                print('Validation mean cross-entropy:', val_loss)
+                print('Validation mean cross-entropy:', val_loss.item())
+            
+            # if val_loss <= best_val_loss:
+            #     epochs_with_no_improvement = 0
+            # else:
+            #     epochs_with_no_improvement += 1
+            
+            # if epochs_with_no_improvement == stop_threshold:
+            #     break
     
     model.load_state_dict(torch.load(save_path))
     return train_losses, val_losses
     
 
 if __name__ == '__main__':
-    seed = 0
-
     from src.data import Corpus, get_data, get_vocabulary, get_labels
+    from src.preprocessing import clean_text, build_adj_matrix
+    from src.models.gcn import GCN
+    
+    seed = 0
+    val_split = 0.1
+
     vocab = get_vocabulary('data/20ng-vocabulary.txt')
     labels = get_labels('data/20ng-labels.txt')
     corpus = get_data('data/train-20news.txt', labels)
     test_corpus = get_data('data/test-20news.txt', labels)
 
-    from src.preprocessing import mask_unknown_words, build_adj_matrix
-
     # Mask out unknown words
-    mask_unknown_words(corpus, vocab)
-    mask_unknown_words(test_corpus, vocab)
+    clean_text(corpus, vocab)
+    clean_text(test_corpus, vocab)
 
-    from src.models.gcn import GCN
+    # Split validation set
+    corpus.shuffle(seed)
+    len_train = int(len(corpus) * (1 - val_split))
+    train_corpus = Corpus(corpus[:len_train])
+    val_corpus = Corpus(corpus[len_train:])
+
+    num_documents = len(train_corpus) + len(val_corpus) + len(test_corpus)
+    train_adj_matrix = build_adj_matrix(train_corpus, vocab, num_documents, doc_offset=0)
+    val_adj_matrix = build_adj_matrix(val_corpus, vocab, num_documents, doc_offset=len(train_corpus))
+    test_adj_matrix = build_adj_matrix(test_corpus, vocab, num_documents, doc_offset=len(train_corpus) + len(val_corpus))
+
     hidden_size = 200  # hyperparameter
     dropout = 0.5  # hyperparameter
 
-    num_vertices = len(vocab) + len(corpus)
-    model = GCN(num_vertices, hidden_size, 2, len(vocab), dropout=dropout)
+    num_vertices = len(vocab) + num_documents
+    model = GCN(num_vertices, hidden_size, len(labels), len(vocab), dropout=dropout)
 
-    train_losses, val_losses = train(model, corpus, epochs=10, seed=seed, plot_every=1)
+    print('Start training')
+    train_losses, val_losses = train(model, train_adj_matrix, val_adj_matrix, train_corpus.labels(), val_corpus.labels(), len(vocab), epochs=10, seed=seed, plot_every=1)
     print(train_losses)
     print(val_losses)
 
-    test_adj = build_adj_matrix(test_corpus, vocab)
-    test_loss = evaluate(model, test_adj, test_corpus.labels(), nn.CrossEntropyLoss())
+    test_start_idx = vocab_size + train_size + val_size
+    test_loss = evaluate(model, test_adj_matrix, test_corpus.labels(), nn.CrossEntropyLoss(), start_idx=test_start_idx)
     print(test_loss)