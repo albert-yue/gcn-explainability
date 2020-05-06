f954ed8e04162de07d2a480b4643e31a70a2e9e3
Albert Yue <ayue@mit.edu>
2020-05-06 07:48:36 -0400

    Fix for type

--- src/treebank_train.py
+++ src/treebank_train.py
@@ -59,7 +59,7 @@ def train(model, train_dataset, val_dataset, batch_size=50, epochs=200, init_lr=
 
         train_losses.append(epoch_loss / num_batches)
         if epoch % print_every == 0:
-            print('Train mean cross-entropy:', epoch_loss.item() / num_batches)
+            print('Train mean cross-entropy:', epoch_loss / num_batches)
 
         if epoch % val_every == 0:
             val_loss = evaluate(model, val_dataset, val_dataset.labels(), loss_fn)
